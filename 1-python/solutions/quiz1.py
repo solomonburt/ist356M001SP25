62fb81aa22337d2dc1ae873474c3e43c397d166c
ist300 <auramnar@g.syr.edu>
2025-02-12 15:57:58 -0500

    clean up

--- 1-python/solutions/quiz1.py
+++ 1-python/solutions/quiz1.py
@@ -1,15 +1,9 @@
 
 #Q5
-assert isinstance(area(1), float), "Test 1 failed"
-assert area(0) == 0, "Test 2 failed"
+
 
 #Q6
-lyst = [1, 2, 3, 4, 5]
-test = [x ** 2 for x in lyst]
-print(test)
+
 
 
 #Q7
-email = "tomas.beuzen@fakemail.com"
-domain = email.split('@')[-1].split('.com')[0]
-print(domain)
