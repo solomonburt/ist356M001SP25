b9ce87c20eafd1458cf19ca08b190d725c21fcd3
ist300 <auramnar@g.syr.edu>
2025-03-17 05:43:19 -0400

    Add initial implementations for solutions 3-4-1 and 3-4-2

--- 3-data/solutions/3-3-1.py
+++ 3-data/solutions/3-3-1.py
@@ -7,11 +7,21 @@ response = requests.get("https://raw.githubusercontent.com/mafudge/datasets/refs
 employees = response.json()
 st.write(employees) # this will show the dictionary
 
-df = pd.DataFrame(employees['accounting'])
-st.dataframe(df)
+departments= []
+for dept_name in employees.keys():
+    dept_employees=pd.json_normalize(employees, record_path=dept_name)
+    #st.write(dept_employees)
+    dept_employees['dept']=dept_name
+    departments.append(dept_employees)
+    
+    
+combined = pd.concat(departments, ignore_index=True)
+st.dataframe(combined)
 
-for key in employees.keys():
-    st.write(f"Key: {key}")
+
+    
+ 
 
 
-st.write(employees) # this will show the dictionary
+
+
---
+++ 3-data/solutions/3-3-2.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+base = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/minimart/"
+months = ['jan', 'feb', 'mar', 'apr']
+
+st.title("Who's not Buying from MiniMart?")
+month = st.selectbox('Select Month:', months)
+
+purchases = pd.read_csv(f"{base}/purchases-{month}.csv")
+customers = pd.read_csv(f"{base}/customers.csv")
+combined = pd.merge(customers, purchases, left_on='customer_id', right_on= 'customer_id', how='left')
+st.dataframe(combined)
+cols= ['customer_id', 'firstname', 'lastname']
+did_not_buy = combined["order_id"].isnull()
+customers_who_did_not_buy = combined[did_not_buy][cols]
+st.dataframe(customers_who_did_not_buy, hide_index=True)
+
+
+
